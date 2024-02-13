cf68283bea73befd20382021a4ffefb4fa8ec6d8
Akshadgod <157225495+Akshadgod@users.noreply.github.com>
2024-02-13 20:15:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ def is_enabled(value, default):
 API_ID = int(environ.get("API_ID", "27981265"))
 API_HASH = environ.get("API_HASH", "e2e52a1550b8dea246b64617a768eee2")
 BOT_TOKEN = environ.get("BOT_TOKEN", "6831175612:AAEO_xY86nFwe-1PZl719vRPnc68RGgFIjs")
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "1002093018566"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002093018566"))
 ADMINS = int(environ.get("ADMINS", "5236678934"))
 DB_URI = environ.get("DB_URI", "mongodb+srv://Akshadgod:14643583pp@cluster0.nnyqicw.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = environ.get("DB_NAME", "chatgptvjbot")
