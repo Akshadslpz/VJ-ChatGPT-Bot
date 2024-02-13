86fd3a86c5fd67ba39094f3703557bbd51bdd127
Akshadgod <157225495+Akshadgod@users.noreply.github.com>
2024-02-13 19:53:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ def is_enabled(value, default):
     else:
         return default
 
-API_ID = int(environ.get("API_ID", "2798126"))
-API_HASH = environ.get("API_HASH", "e2e52a1550b8dea246b64617a768ee")
+API_ID = int(environ.get("API_ID", "27981265"))
+API_HASH = environ.get("API_HASH", "e2e52a1550b8dea246b64617a768eee2")
 BOT_TOKEN = environ.get("BOT_TOKEN", "6831175612:AAEO_xY86nFwe-1PZl719vRPnc68RGgFIjs")
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "1001923947029"))
 ADMINS = int(environ.get("ADMINS", "5236678934"))
